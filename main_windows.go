1abeb2be796087423f2fa8738d144d7ea1610a15
chn.lee.yumi <chn.lee.yumi@gmail.com>
2019-06-24 23:42:15 +0800

    增加热键说明

--- main_windows.go
+++ main_windows.go
@@ -11,7 +11,7 @@ func main() {
 		fmt.Println("欢迎使用GDUT网络测试工具！")
 		fmt.Println("1.单次测试（约需一分钟）")
 		fmt.Println("2.持续测试（需要手动关闭窗口来终止）")
-		fmt.Println("提示：可以按Ctrl+Shift+H隐藏窗口，按Ctlr+Shift+S显示窗口。")
+		fmt.Println("提示：可以按Ctrl+Shift+H隐藏窗口，按Ctlr+Shift+S显示窗口（可能需要等半分钟才生效）")
 		go HotkeyHandel()
 		fmt.Printf("请输入数字并按回车：")
 		var choice int
